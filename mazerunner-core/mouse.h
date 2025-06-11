29a751f3aaf965190406ce6a8b0f8ad5eb151cbb
Neil Stevenson <neil.github@secureness.co.uk>
2025-06-11 16:25:42 +0100

    Add check for blocked route to target during search.

--- mazerunner-core/mouse.h
+++ mazerunner-core/mouse.h
@@ -476,6 +476,17 @@ class Mouse {
       update_map();
       maze.flood(target);
       unsigned char newHeading = maze.heading_to_smallest(m_location, m_heading);
+      if(newHeading == BLOCKED)
+      {
+        motion.stop();
+        sensors.set_steering_mode(STEERING_OFF);
+        sensors.disable();
+        motion.disable_drive();
+        SerialPort.println();
+        SerialPort.println(F("NO ROUTE TO TARGET!"));
+        panic();
+        return;
+      }
       unsigned char hdgChange = (newHeading - m_heading) & 0x3;
       if (m_location != target) {
         switch (hdgChange) {
